f4593822c4ad97e1d70c1c9f2310140b26c861ee
Gnimel <7000466+Gnimel@users.noreply.github.com>
2020-03-16 20:36:42 +0100

    Add files via upload

--- m3u8.sh
+++ m3u8.sh
@@ -5,7 +5,7 @@ wget -nc `sed -n '/EXT-X-SESSION-KEY/'p m.m3u8 | sed 's/.*URI="\(.*\)".*/\1/g'` 
 sed 's/".*"/"encryption.key"/g' index.m3u8 > index-offline.m3u8
 WEB=`sed -n '/^http/'p m.m3u8 | sed -n '$'p | sed 's/ism.*/ism\//g'`
 sed -n '/ts$/p' index.m3u8 | awk -v c="$WEB" '{print c$0}' > download.txt
-aria2c -c -j 3 -i download.txt
-#wget -nc -i download.txt -q --show-progress
-ffmpeg -allowed_extensions ALL -i index-offline.m3u8 -c copy m.mp4
+#wget -nc -i download.txt -q --show-progress && \
+aria2c -c -j 3 -i download.txt && \
+ffmpeg -allowed_extensions ALL -i index-offline.m3u8 -c copy m.mp4 && \
 rm index.m3u8 index-offline.m3u8 encryption.key download.txt
